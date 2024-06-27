7e96dc088adf936bbced4279cd03ce6ad60fb9d1
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-06-27 22:42:56 +0300

    tailwind + clsx

--- crm-system/src/app/layout.tsx
+++ crm-system/src/app/layout.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './globals.css';
-import ActiveLable from './components/active-lable';
-import NotActiveLable from './components/not-active-lable';
+import StstusLabel from './components/status-lable';
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
-      <ActiveLable>Active</ActiveLable>
-      <NotActiveLable>Not active</NotActiveLable>
     </html>
   );
 }
--- crm-system/src/app/page.tsx
+++ crm-system/src/app/page.tsx
@@ -1,7 +1,13 @@
+import StstusLabel, { Status } from "./components/status-lable";
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <StstusLabel status={Status.Active}>Active</StstusLabel>
+      <StstusLabel status={Status.NotActive}>Not active</StstusLabel>
+      <StstusLabel status={Status.Pending}>Pending</StstusLabel>
+      <StstusLabel status={Status.Suspended}>Suspended</StstusLabel>
     </main>
   );
 }
