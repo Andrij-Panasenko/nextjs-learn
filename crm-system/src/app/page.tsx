2ae569583307d8de08a1a9c2b3feda48dd9e5362
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-06-28 23:12:36 +0300

    modal

--- crm-system/src/app/page.tsx
+++ crm-system/src/app/page.tsx
@@ -1,13 +1,10 @@
-import StstusLabel, { Status } from "./components/status-lable";
+import AddCompanyButton from "./components/add-company-button";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StstusLabel status={Status.Active}>Active</StstusLabel>
-      <StstusLabel status={Status.NotActive}>Not active</StstusLabel>
-      <StstusLabel status={Status.Pending}>Pending</StstusLabel>
-      <StstusLabel status={Status.Suspended}>Suspended</StstusLabel>
+      <AddCompanyButton/>
     </main>
   );
 }
