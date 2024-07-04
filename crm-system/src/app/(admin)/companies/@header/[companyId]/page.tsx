4b79e69efe93490cbf70d69347bd912b50ce60bb
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-07-04 22:35:31 +0300

    Update page.tsx

--- crm-system/src/app/(admin)/companies/@header/[companyId]/page.tsx
+++ crm-system/src/app/(admin)/companies/@header/[companyId]/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Header from '@/app/components/header';
 
 export interface PageProps {
-  params: { id: string };
+  params: { companyId: string };
 }
 
 export default function Page({ params }: PageProps) {
-  return <Header>{`Company (${params.id})`}</Header>;
+  return <Header>{`Company (${params.companyId})`}</Header>;
 }
