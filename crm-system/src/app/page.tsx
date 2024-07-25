7cd7d10ba182e7c150564755473c829fff4df6cf
VladProtchenko <andrii.panasenko295@gmail.com>
2024-07-25 18:17:44 +0300

    mvp

--- crm-system/src/app/page.tsx
+++ crm-system/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+import Button from './components/button';
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
+      <Link href="/dashboard">
+        <Button>To dashboard</Button>
+      </Link>
+      <Link href="/companies">
+        <Button>To companies</Button>
+      </Link>
     </main>
   );
 }
