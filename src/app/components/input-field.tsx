f603da855eaf0076f573d7f34e80cc877932df97
VladProtchenko <andrii.panasenko295@gmail.com>
2024-07-25 18:53:08 +0300

    Update input-field.tsx

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -3,9 +3,9 @@
 import React from 'react';
 import { Field } from 'formik';
 
-export interface InputFieldProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  as?: string;
 }
 
 export default function InputField({ label, id, ...rest }: InputFieldProps) {
